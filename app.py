6b5a0efb7fa936975c3555c88fbc82f8ab54c8ed
Raket Yonex <154741306+raketyonex@users.noreply.github.com>
2025-04-22 18:05:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ with b1:
     img = st.file_uploader("**Skin Image**", type=['jpg','png'])
     st.write("")
 with b2:
-    type = st.radio("**Model Type**", options=['.pt','_fp32.onnx','_fp16.onnx','_int8.onnx'], horizontal=True)
+    st.write("press to start!")
     button = st.button("**Identification**", type="primary", use_container_width=True)
 
 if button:
